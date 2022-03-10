26a7726aff03e85ffd2619227819df9d82f134c4
Simon-4 <52985243+Simon-4@users.noreply.github.com>
2022-03-10 17:00:20 -0300

    Add hook's useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import { Tweet } from "./Components/Tweet"
 
 function App() {
+  const [tweets, setTweets] = useState<string[]>([
+    'Tweet 1',
+    'Tweet 2',
+    'Tweet 3',
+    'Tweet 4',
+  ])
+
   return (
     <div>
-      <Tweet text="Tweet 1"/>
-      <Tweet text="Tweet 2"/>
-      <Tweet text="Tweet 3"/>
+      {tweets.map(tweet => {
+        return <Tweet text={tweet} />
+      })}
+
+      <button>Adicionar tweet</button>
     </div>
   );
 }
