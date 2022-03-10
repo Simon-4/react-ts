05aa9886add9bc5e89a03d66e4a7012f9313bc98
Simon-4 <52985243+Simon-4@users.noreply.github.com>
2022-03-10 16:20:47 -0300

    Initial Commit - React using TS and Vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Tweet } from "./Components/Tweet"
+
+function App() {
+  return (
+    <div>
+      <Tweet text="Tweet 1"/>
+      <Tweet text="Tweet 2"/>
+      <Tweet text="Tweet 3"/>
+    </div>
+  );
+}
+
+export default App
